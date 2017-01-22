b5d2f86906a739b36fae7f20b204c2b903a73d6c
yostane <yostane@yahoo.fr>
2017-01-22 21:17:27 +0100

    1st commit

---
+++ SimpleBleCentral/ViewController.swift
@@ -0,0 +1,206 @@
+//
+//  ViewController.swift
+//  SimpleBleCentral
+//
+//  Created by yassine benabbas on 22/01/2017.
+//  Copyright © 2017 yassine benabbas. All rights reserved.
+//
+
+import UIKit
+import BlueCapKit
+import CoreBluetooth
+
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    public enum AppError : Error {
+        case dataCharactertisticNotFound
+        case enabledCharactertisticNotFound
+        case updateCharactertisticNotFound
+        case serviceNotFound
+        case invalidState
+        case resetting
+        case poweredOff
+        case unknown
+    }
+    @IBOutlet weak var loadingView: UIView!
+    @IBOutlet weak var characteristicView: UIView!
+    @IBOutlet weak var connectionStatusLabel: UILabel!
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var valueToWriteTextField: UITextField!
+    @IBOutlet weak var notifiedValueLabel: UILabel!
+    
+    var dataCharacteristic : Characteristic?
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let serviceUUID = CBUUID(string:"ec00")
+        var peripheral: Peripheral?
+        let dateCharacteristicUUID = CBUUID(string:"ec0e")
+        
+        //initialize a central manager with a restore key. The restore key allows to resuse the same central manager in future calls
+        let manager = CentralManager(options: [CBCentralManagerOptionRestoreIdentifierKey : "CentralMangerKey" as NSString])
+        
+        //A future stram that notifies us when the state of the central manager changes
+        let stateChangeFuture = manager.whenStateChanges()
+        
+        //handle state changes and return a scan future if the bluetooth is powered on.
+        let scanFuture = stateChangeFuture.flatMap { state -> FutureStream<Peripheral> in
+            switch state {
+            case .poweredOn:
+                DispatchQueue.main.async {
+                    self.connectionStatusLabel.text = "start scanning"
+                    print(self.connectionStatusLabel.text!)
+                }
+                //scan for peripherlas that advertise the ec00 service
+                return manager.startScanning(forServiceUUIDs: [serviceUUID])
+            case .poweredOff:
+                throw AppError.poweredOff
+            case .unauthorized, .unsupported:
+                throw AppError.invalidState
+            case .resetting:
+                throw AppError.resetting
+            case .unknown:
+                //generally this state is ignored
+                throw AppError.unknown
+            }
+        }
+        
+        scanFuture.onFailure { error in
+            guard let appError = error as? AppError else {
+                return
+            }
+            switch appError {
+            case .invalidState:
+                break
+            case .resetting:
+                manager.reset()
+            case .poweredOff:
+                break
+            case .unknown:
+                break
+            default:
+                break;
+            }
+        }
+        
+        //We will connect to the first scanned peripheral
+        let connectionFuture = scanFuture.flatMap { peripheral -> FutureStream<Peripheral> in
+            //stop the scan as soon as we find the first peripheral
+            manager.stopScanning()
+            DispatchQueue.main.async {
+                self.connectionStatusLabel.text = "Found peripheral \(peripheral.identifier.uuidString). Trying to connect"
+                print(self.connectionStatusLabel.text!)
+            }
+            //connect to the peripheral in order to trigger the connected mode
+            return peripheral.connect(connectionTimeout: 10, capacity: 5)
+        }
+        
+        //we will next discover the "ec00" service in order be able to access its characteristics
+        let discoveryFuture = connectionFuture.flatMap { peripheral -> Future<Peripheral> in
+            return peripheral.discoverServices([serviceUUID])
+            }.flatMap { discoveredPeripheral -> Future<Service> in
+                guard let service = discoveredPeripheral.service(serviceUUID) else {
+                    throw AppError.serviceNotFound
+                }
+                peripheral = discoveredPeripheral
+                DispatchQueue.main.async {
+                    self.connectionStatusLabel.text = "Discovered service \(service.uuid.uuidString). Trying to discover characteristics"
+                    print(self.connectionStatusLabel.text!)
+                }
+                //we have discovered the service, the next step is to discover the "ec0e" characteristic
+                return service.discoverCharacteristics([dateCharacteristicUUID])
+        }
+        
+        /**
+         1- checks if the characteristic is correctly discovered
+         2- Register for notifications using the dataFuture variable
+        */
+        let dataFuture = discoveryFuture.flatMap { service -> Future<Characteristic> in
+            guard let dataCharacteristic = service.characteristic(dateCharacteristicUUID) else {
+                throw AppError.dataCharactertisticNotFound
+            }
+            self.dataCharacteristic = dataCharacteristic
+            DispatchQueue.main.async {
+                self.connectionStatusLabel.text = "Discovered characteristic \(dataCharacteristic.uuid.uuidString). COOL :)"
+                print(self.connectionStatusLabel.text!)
+            }
+            //when we successfully discover the characteristic, we can show the characteritic view
+            DispatchQueue.main.async {
+                self.loadingView.isHidden = true
+                self.characteristicView.isHidden = false
+            }
+            //read the data from the characteristic
+            self.read()
+            //Ask the characteristic to start notifying for value change
+            return dataCharacteristic.startNotifying()
+            }.flatMap { characteristic -> FutureStream<(characteristic: Characteristic, data: Data?)> in
+                //regeister to recieve a notifcation when the value of the characteristic changes and return a future that handles these notifications
+                return characteristic.receiveNotificationUpdates(capacity: 10)
+        }
+        
+        //The onSuccess method is called every time the characteristic value changes
+        dataFuture.onSuccess { (_, data) in
+            let s = String(data:data!, encoding: .utf8)
+            DispatchQueue.main.async {
+                self.notifiedValueLabel.text = "notified value is \(s)"
+            }
+        }
+        
+        //handle any failure in the previous chain
+        dataFuture.onFailure { error in
+            switch error {
+            case PeripheralError.disconnected:
+                peripheral?.reconnect()
+            case AppError.serviceNotFound:
+                break
+            case AppError.dataCharactertisticNotFound:
+                break
+            default:
+                break
+            }
+        }
+    }
+    
+    @IBAction func onWriteTapped(_ sender: Any) {
+        self.write()
+    }
+    
+    @IBAction func onRefreshTap(_ sender: Any) {
+        self.read()
+    }
+    
+    func read(){
+        //read a value from the characteristic
+        let readFuture = self.dataCharacteristic?.read(timeout: 5)
+        readFuture?.onSuccess { (_) in
+            //the value is in the dataValue property
+            let s = String(data:(self.dataCharacteristic?.dataValue)!, encoding: .utf8)
+            DispatchQueue.main.async {
+                self.valueLabel.text = "Read value is \(s)"
+                print(self.valueLabel.text!)
+            }
+        }
+        readFuture?.onFailure { (_) in
+            self.valueLabel.text = "read error"
+        }
+    }
+    
+    func write(){
+        self.valueToWriteTextField.resignFirstResponder()
+        guard let text = self.valueToWriteTextField.text else{
+            return;
+        }
+        //write a value to the characteristic
+        let writeFuture = self.dataCharacteristic?.write(data:text.data(using: .utf8)!)
+        writeFuture?.onSuccess(completion: { (_) in
+            print("write succes")
+        })
+        writeFuture?.onFailure(completion: { (e) in
+            print("write failed")
+        })
+    }
+    
+}
+
